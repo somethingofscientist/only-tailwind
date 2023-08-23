4952ac5e30602fb3b5110cf039bea394c821f6e4
abhinav-siwal <1shraddhanand@gmail.com>
2023-08-23 10:41:33 +0530

    start learning tailwind css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
